1e32d709060e3626afc7d3ee29f52e20b30a1124
drmth <79118141+drmth@users.noreply.github.com>
2022-06-30 21:47:27 +0200

    add book with format type

--- addBook.php
+++ addBook.php
@@ -7,15 +7,26 @@ $statement = $pdo->query($query);
 $authors = $statement->fetchAll();
 sort($authors);
 
+$query = "SELECT * FROM format_source";
+$statement = $pdo->query($query);
+$formats = $statement->fetchAll();
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     global $pdo;
+    $query_format = "";
     $author_id = $_POST['authors_list'];
+    $format_selected = $_POST['format'];
     $image = $_POST['image'];
     $title = $_POST['title'];
     $sth = $pdo->prepare("INSERT INTO book (title, image, author_id) VALUES (:title, :image, $author_id)");
     $sth->bindValue('image', $image, PDO::PARAM_STR);
     $sth->bindValue('title', $title, PDO::PARAM_STR);
     $sth->execute();
+    $id = $pdo->lastInsertId();
+    foreach ($format_selected as $format) {
+        $query_format .= "INSERT INTO format_bind(book_id, format_id) VALUES ($id, $format);";
+    }
+    $statement = $pdo->query($query_format);
     header('Location: biblio.php');
 }
 
@@ -52,8 +63,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <option value="<?php echo $author['id']?>">
                         <?php echo $author['first_name'] . ' ' . $author['last_name']?>
                     </option>
-                <?php endforeach; ?>
+                <?php endforeach ?>
             </select>
+        </div>
+        <div style="padding: 10px; padding-top: 20px;">
+            <p>Formats disponibles : </p>
+        </div>
+        <div style="padding: 10px;">
+            <?php foreach ($formats as $format) :?>
+                <input type="checkbox" id=<?php echo $format['type'] ?> value=<?php echo $format['id'] ?> name="format[]">
+                <label for=<?php echo $format['type'] ?>><?php echo $format['type'] ?></label>
+            <?php endforeach ?>
         </div>
         <div class="button" style="padding: 10px;">
             <input type="submit" name="action" value="Ajouter"></input>
